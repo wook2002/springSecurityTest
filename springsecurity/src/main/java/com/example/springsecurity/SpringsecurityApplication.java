32e807eddc3b11fd0e3794ff246936014292f942
ejejej2002 <ejejej2002@gmail.com>
2023-03-01 13:45:14 +0900

    sdfsdfdsf

---
+++ springsecurity/src/main/java/com/example/springsecurity/SpringsecurityApplication.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityApplication {
+
+	public static void main(String[] args) {
+		System.out.println("=======start1=======");
+		SpringApplication.run(SpringsecurityApplication.class, args);
+		System.out.println("=======start2=======");
+	}
+
+}
